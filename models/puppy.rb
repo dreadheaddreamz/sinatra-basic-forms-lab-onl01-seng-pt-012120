fa944e953c0f36ae503ec3f37168aa7014c55bea
James Batsell <jamesbatsell05@gmail.com>
2020-04-21 03:00:23 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,10 +1,21 @@
-class Puppy
-  
-  attr_accessor :name, :breed, :age
-  
-  def initialize(name, breed, age)
-    @name = name
-    @breed = breed
-    @age = age
+require_relative 'config/environment'
+
+class App < Sinatra::Base
+
+  get '/' do
+    erb :index
   end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/puppy' do
+    name = params[:name]
+    breed = params[:breed]
+    age = params[:age]
+    @puppy = Puppy.new(name,breed,age)
+    erb :display_puppy
+  end
+
 end
